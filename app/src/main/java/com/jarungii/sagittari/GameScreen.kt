54ae860cccf7696138fac93f797c307e48798edd
jarungii <tannokrr@gmail.com>
2024-10-28 02:08:15 +0400

    Initial commit

---
+++ app/src/main/java/com/jarungii/sagittari/GameScreen.kt
@@ -0,0 +1,27 @@
+package com.jarungii.sagittari
+
+import android.os.Bundle
+import android.view.GestureDetector
+import android.view.GestureDetector.OnGestureListener
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GameScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_game_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/jarungii/sagittari/TitleScreen.kt
@@ -0,0 +1,31 @@
+package com.jarungii.sagittari
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TitleScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_title_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val startButton: Button = findViewById(R.id.startButton)
+        startButton.setOnClickListener{
+            val intent: Intent = Intent(this, GameScreen::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
